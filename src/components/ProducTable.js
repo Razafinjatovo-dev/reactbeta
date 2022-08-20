81cf90be258630e83794718679f50dd0a4af9207
Joel RAZAFINJATOVO <joel.razafinjatovo@epitech.eu>
2022-08-20 09:54:05 +0200

    listing fruits ok

--- src/components/ProducTable.js
+++ src/components/ProducTable.js
@@ -1,29 +1,61 @@
-import ProductCategoryRow from './ProductCategoryRow'
-import ProductRow from './ProductRow';
-const list =
-    [
-        { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
-        { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
-        { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
-        { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
-        { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
-        { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
-    ];
+import ProductCategoryRow from "./ProductCategoryRow";
+import ProductRow from "./ProductRow";
+const list = [
+  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" },
+];
+
+let lastCategory = null;
+
+const productsToDisplay = list.map((product) => {
+  if (product.category === lastCategory) {
+    return <ProductRow product={product} />;
+  } else {
+    lastCategory = product.category;
+    return (
+      <>
+        <ProductCategoryRow category={product.category} />
+        <ProductRow product={product} />
+      </>
+    );
+  }
+});
 
 const ProductTable = () => {
-    return (
-        <div class='productTable-container'>
-            <table>
-                <tr>
-                    <th>Name</th>
-                    <th>Price</th>
-                </tr>
-            </table>
-            <ProductCategoryRow category='vegetables' />
-            <ProductRow product={{ category: "Fruits", price: "$1", stocked: true, name: "Apple" }} />
-            <ProductRow product={{ category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" }} />
-        </div>
-    )
-}
+  return (
+    <div class="productTable-container">
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </table>
+
+      {/* <ProductCategoryRow category="vegetables" />
+      <ProductRow
+        product={{
+          category: "Fruits",
+          price: "$1",
+          stocked: true,
+          name: "Apple",
+        }}
+      />
+      <ProductRow
+        product={{
+          category: "Fruits",
+          price: "$2",
+          stocked: false,
+          name: "Passionfruit",
+        }}
+      /> */}
+
+      {productsToDisplay}
+    </div>
+  );
+};
 
 export default ProductTable;
